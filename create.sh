237741dc0694cbe88e2ac3800f230ebdae77e3e5
Sylvain Viart <sylvain303@github>
2017-03-25 06:23:59 +0100

    first create email version beta

--- create.sh
+++ create.sh
@@ -5,55 +5,158 @@
 # Usage: ./create.sh USERNAME URL
 #
 
-user="$1"
-url="$2"
-
-if [[ -z "$user" ]]
+[[ $0 != "$BASH_SOURCE" ]] && sourced=1 || sourced=0
+if [[ $sourced -eq 0  ]]
 then
-    echo "missing user"
-    exit 1
+  ME=$(readlink -f $0)
+else
+  ME=$(readlink -f "$BASH_SOURCE")
 fi
+SCRIPTDIR=$(dirname $ME)
+OVH_CLIDIR=$HOME/code/ovh/ovh-cli
 
-if [[ -z "$url" ]]
-then
-    echo "missing url"
-    exit 1
-fi
+ovh_cli() {
+  cd $OVH_CLIDIR
+  ./ovh-eu "$@"
+  local r=$?
+  cd - > /dev/null
+  return $r
+}
 
-template="
-user: $user
-email: $user
+ovh_login() {
+  cd $OVH_CLIDIR/../ovh-tools
+  ./mk_cred.py update
+  local r=$?
+  cd - > /dev/null
+  return $r
+}
+
+show_ovh_alias() {
+  email=$1
+  domain=${email#*@}
+  ovh_cli \
+    email-domain $domain redirection --from "$email" \
+    | grep --color $email
+  return $?
+}
+
+check_ovh_email() {
+  email=$1
+  ovh_cli \
+    email-domain ledragon.net redirection --from "$email" \
+    | grep -q "$email"
+  return $?
+}
+
+create_ovh_email() {
+  email=$1
+  domain=${email#*@}
+
+  if grep -l -q "^$domain$" domain_managed.txt
+  then
+    ovh_cli email-domain $domain redirection post \
+        --from $email \
+        --to postmaster@ledragon.net \
+        --localCopy no
+    return $?
+  else
+    return 0
+  fi
+}
+
+remove_ovh_email() {
+  email=$1
+  domain=${email#*@}
+  id=$(show_ovh_alias $email | awk '{print $4}')
+
+  echo "id=$id"
+  if [[ ! -z "$id" ]]
+  then
+    ovh_cli email-domain ${domain} redirection ${id} delete
+  fi
+}
+
+create_password_entry() {
+  email="$1"
+  url="$2"
+
+  if [[ -z "$email" ]]
+  then
+      echo "missing email"
+      exit 1
+  fi
+
+  if [[ -z "$url" ]]
+  then
+      echo "missing url"
+      exit 1
+  fi
+
+  # checkovh email
+  if ! check_ovh_email "$email"
+  then
+    create_ovh_email "$email"
+  fi
+
+  template="
+user: $email
+email: $email
 date: $(date "+%d/%m/%Y")
 type: web
 url: $url
 "
 
-regexp='https?://([^/]+)'
-site=${url}
-if [[ "$url" =~ $regexp ]]
-then
-    site=${BASH_REMATCH[1]}
-fi
-# remove slashes
-site=${site//\//}
+  regexp='https?://([^/]+)'
+  SITE=${url}
+  if [[ "$url" =~ $regexp ]]
+  then
+      SITE=${BASH_REMATCH[1]}
+  fi
+  # remove slashes
+  SITE=${SITE//\//}
 
-entry=web/${user}-$site
-echo "$(pwqgen)$template" | pass insert -m $entry
-pass edit $entry
-
-tmp=~/tmp/new_identity.txt
-# catch email or user
-pass show $entry | grep -E '^(email|user|date|url):' > $tmp 
-identity="/home/sylvain/Notebooks/Notes/Home/identité.txt"
-
-new_entry() {
-    echo -e "\n===== $site ====="
-    grep -E '^(email|date|url):' $tmp
-    echo "''pass show $entry''"
+  ENTRY=web/$SITE
+  echo "$(pwqgen)$template" | pass insert -m $ENTRY
+  pass edit $ENTRY
+  # file exists + size > 0
+  test -s ~/.password-store/$ENTRY.gpg
+  r=$?
+  echo $r
+  return $r
 }
 
-tmp2=~/tmp/new_entry.txt
-# merge
-cat $identity <(new_entry) > $tmp2
-mv $tmp2 $identity
-rm -f $tmp $tmp2
+# need globals: $ENTRY, $SITE and $TMP
+# TMP set by update_identity_zim_wiki()
+# other by create_password_entry()
+new_zim_entry() {
+  echo -e "\n===== $SITE ====="
+  grep -E '^(email|date|url):' $TMP
+  echo "''pass show $ENTRY''"
+}
+
+update_identity_zim_wiki() {
+  TMP=~/tmp/new_identity.txt
+  # catch email or user
+  pass show $ENTRY | grep -E '^(email|user|date|url):' > $TMP
+  identity="/home/sylvain/Notebooks/Notes/Home/identité.txt"
+  tmp2=~/tmp/new_entry.txt
+  # merge into $tmp2
+  cat $identity <(new_zim_entry) > $tmp2
+  diff -u $identity $tmp2
+  mv $tmp2 $identity &&  echo "$identity updated + $SITE"
+  # cleanup
+  rm -f $TMP $tmp2
+  # debug
+  #echo $TMP
+}
+
+create_main() {
+  if create_password_entry $1 "$2"
+  then
+    update_identity_zim_wiki
+  fi
+}
+
+if [[ $sourced -eq 0  ]] ; then
+  create_main "$@"
+fi
